bd9befd2ab84a4e256daa690e27cdc52bba3ce2e
ES <eon@earncef.com>
2019-02-21 09:59:31 +0530

    Update and.test.js

--- tests/and.test.js
+++ tests/and.test.js
@@ -1,4 +1,3 @@
-
 import when from '../src/index';
 
 describe('and', () => {
@@ -17,34 +16,4 @@ describe('and', () => {
         expect(when(['and', ['name', 'John Doe', 'isNot'], ['age', 17, 'isNot']], data)).to.equal(false)
         expect(when(['and', ['name', 'John', 'isNot'], ['age', 17, 'isNot']], data)).to.equal(true)
     })
-
-    // it('single rule using "or"', () => {
-    //     expect(when(['or', ['name', 'John Doe', 'is']], data)).to.equal(true)
-    //     expect(when(['or', ['name', 'John', 'is']], data)).to.equal(false)
-    // })
-    //
-    // it('deep object key', () => {
-    //     let data = {
-    //         contact: {
-    //             person: {
-    //                 name: 'John Doe'
-    //             }
-    //         }
-    //     }
-    //     expect(when(['contact.person.name', 'John Doe', 'is'], data)).to.equal(true)
-    //     expect(when(['contact.person.lastName', 'John Doe', 'is'], data)).to.equal(false)
-    // })
-    //
-    // it('deep object array key', () => {
-    //     let data = {
-    //         contact: {
-    //             person: [{
-    //                 name: 'John Doe'
-    //             }]
-    //         }
-    //     }
-    //     expect(when(['contact.person.0.name', 'John Doe', 'is'], data)).to.equal(true)
-    //     expect(when(['contact.person[0].name', 'John Doe', 'is'], data)).to.equal(true)
-    //     expect(when(['contact.person[1].name', 'John Doe', 'is'], data)).to.equal(false)
-    // })
 })
